9996db3e94216df3dbede8d49d7f2c986a734368
Alvis HT Tang <alvis@hilbert.space>
2026-07-20 21:08:16 +0100

    feat: add public API barrel export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export type {};
+export * from '#render';
+
+export { assembleStack, disassembleStack } from '#stack';
+export type {
+  StackBlock,
+  StackDescriptionBlock,
+  StackLocationBlock,
+} from '#stack';
+
+export { createSourceResolver } from '#source';
+export type { SourceResolution, SourceResolver } from '#source';
+
+export { highlight } from '#highlight';
+export type { HighlightTheme } from '#highlight';
